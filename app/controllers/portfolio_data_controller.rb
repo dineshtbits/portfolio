162eb90823b3228d2a4cca7e3c425346e622b42e
Dinesh <dinesh@Suryas-MacBook-Pro.local>
2021-10-07 22:26:55 -0500

    implemeted portfolio

--- app/controllers/portfolio_data_controller.rb
+++ app/controllers/portfolio_data_controller.rb
@@ -1,2 +1,6 @@
 class PortfolioDataController < ApplicationController
+
+  def index
+    @portfolio_items = PortfolioDatum.all
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog_id|
+  Blog.create!(
+    title: "My Blog Post #{blog_id}",
+    body: "Body here"
+  )
+end
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilied: 65
+  )
+end
+
+9.times do |p|
+  PortfolioDatum.create!(
+    title: "Portfolio Title #{p}",
+    subtitle: "body",
+    body: "body",
+    main_image: "test",
+    thumb_image: "thumb image"
+  )
+end
