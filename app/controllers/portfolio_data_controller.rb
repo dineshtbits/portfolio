35c7323db6284129732abd519b659e702debf88b
Dinesh <dinesh@Suryas-MacBook-Pro.local>
2021-10-09 05:13:10 -0500

    implemeted portfolio

--- app/controllers/portfolio_data_controller.rb
+++ app/controllers/portfolio_data_controller.rb
@@ -3,4 +3,20 @@ class PortfolioDataController < ApplicationController
   def index
     @portfolio_items = PortfolioDatum.all
   end
+
+  def new
+    @p = PortfolioDatum.new
+  end
+
+  def create
+    @portfolio = PortfolioDatum.new(params.require(:portfolio_datum).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolio_data_path, notice: "Blog was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
 end
